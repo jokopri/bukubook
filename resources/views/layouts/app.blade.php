de2f5c9f59ef1b45c9368a4e1a7d5d177b87602a
Joko Prihantoro <joko.prihantoro@set.wapresri.go.id>
2023-07-21 15:19:45 +0700

    First Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                </li> 
                             @endif
                         @else
                         <li class="nav-item">
